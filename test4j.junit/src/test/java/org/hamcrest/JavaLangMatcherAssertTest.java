8b7e7809e123bb0afc0fea8518cdd544a829682f
hxq8176 <hxq8176@closeli.cn>
2018-06-28 20:04:13 +0800

    test4j-default.properties中的一个字段名有问题修改，以及junit中增加hamcrest的单元测试代码供学习

---
+++ test4j.junit/src/test/java/org/hamcrest/JavaLangMatcherAssertTest.java
@@ -0,0 +1,13 @@
+package org.hamcrest;
+
+import junit.framework.TestCase;
+
+public class JavaLangMatcherAssertTest extends TestCase {
+    /*public void testWouldCauseFailingAssertion() throws Exception {
+        assertEquals(false, that("Foo", startsWith("Bar")));
+    }
+    
+    public void testWouldCausePassingAssertion() throws Exception {
+        assertEquals(true, that("Foo", startsWith("F")));
+    }*/
+}
---
+++ test4j.junit/src/test/java/org/hamcrest/integration/EasyMock2AdapterTest.java
@@ -0,0 +1,36 @@
+package org.hamcrest.integration;
+
+import junit.framework.TestCase;
+
+public class EasyMock2AdapterTest extends TestCase {
+
+    public static interface InterfaceToMock {
+        void doStuff(String name, int number);
+    }
+
+    /*public void testAdaptsHamcrestMatcherToEasyMockArgumentsMatcher() {
+        IArgumentMatcher easyMockMatcher = new EasyMock2Adapter(equalTo("expected"));
+        assertTrue("Should have matched", easyMockMatcher.matches("expected"));
+        assertFalse("Should not have matched", easyMockMatcher.matches("unexpected"));
+    }
+
+    public void testDelegatesDescriptionToUnderlyingMatcher() {
+        IArgumentMatcher easyMockMatcher = new EasyMock2Adapter(new BaseMatcher<Object>() {
+            @Override
+            public boolean matches(Object o) {
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is like ");
+                description.appendValue("cheese");
+            }
+        });
+
+        StringBuffer buffer = new StringBuffer();
+        easyMockMatcher.appendTo(buffer);
+        assertEquals("is like \"cheese\"", buffer.toString());
+    }*/
+
+}
